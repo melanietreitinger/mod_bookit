762d8aa73c2547a23b57e36cac4c08b10c2eeb98
Melanie Treitinger <melanie.treitinger@ruhr-uni-bochum.de>
2024-09-04 15:47:43 +0200

    Add german lang file
    Co-authored-by: Florian Fischer

---
+++ lang/de/bookit.php
@@ -0,0 +1,72 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     mod_bookit
+ * @category    string
+ * @copyright   2024 Melanie Treitinger, Ruhr-Universität Bochum <melanie.treitinger@ruhr-uni-bochum.de>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['bookit:addinstance'] = 'BookIt Instanz hinzufügen';
+$string['bookit:view'] = 'BookIt Instanz anzeigen';
+$string['bookitfieldset'] = 'PLATZHALTER';
+$string['instancename'] = 'Name';
+$string['modulename'] = 'BookIt';
+$string['modulename_help'] = 'BookIt ist ein PlugIn für die Buchung von Services, Prüfungsterminen, Räumen oder anderer Resscourcen.';
+$string['modulename_link'] = 'mod/bookit/view';
+$string['modulenameplural'] = 'BookIt Instanzen';
+$string['pluginadministration'] = 'BookIt Administration';
+$string['pluginname'] = 'BookIt';
+$string['addbooking'] = 'Antrag versenden';
+
+// Booking form.
+$string['event_name'] = 'Event-Name';
+$string['event_name_help'] = 'Tragen Sie den Namen des Events ein.';
+$string['summer_semester'] = 'Sommersemester';
+$string['winter_semester'] = 'Wintersemester';
+$string['select_semester'] = 'Semester';
+$string['select_semester_help'] = 'Wählen Sie das Semester aus, in dem das Event stattfindet';
+$string['event_department'] = 'Institution';
+$string['event_department_help'] = 'Tragen Sie die beantragende Institution ein.';
+$string['event_room'] = 'Raum';
+$string['event_room_help'] = 'Wählen Sie einen Raum für das Event.';
+$string['event_start'] = 'Beginn';
+$string['event_start_help'] = 'Wählen Sie das Startdatum des Events.';
+$string['event_end'] = 'Ende';
+$string['event_end_help'] = 'Wählen Sie das Enddatum und -uhrzeit.';
+$string['event_students'] = 'Studierendenanzahl';
+$string['event_students_help'] = 'Tragen Sie die erwartete Anzahl der Studierenden ein.';
+$string['resource_amount'] = 'Anzahl';
+$string['event_person'] = 'Verantwortliche Person';
+$string['event_person_help'] = 'Tragen Sie die verantwortliche Person ein.';
+$string['event_email'] = 'E-Mail';
+$string['event_email_help'] = 'Tragen Sie die Email der verantwortlichen Person ein.';
+$string['event_duration'] = 'Dauer des Events';
+$string['event_duration_help'] = 'Tragen Sie die Dauer des Events ein.';
+$string['event_compensationfordisadvantages'] = 'Nachteilsausgleich';
+$string['event_compensationfordisadvantages_help'] = 'Tragen Sie hier die bereits bekannten Informationen zu Studierenden mit Nachteilsausgleich ein.';
+$string['edit_event'] = "Event bearbeiten";
+$string['event_notes'] = 'Anmerkungen';
+$string['event_notes_help'] = 'Tragen Sie zusätzliche Informationen für den Support ein.';
+
+// Booking form headings.
+$string['event_resources'] = 'Ressourcen';
+
