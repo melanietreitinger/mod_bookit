ecb2913635c5bf5fb730bc48b6fbbec354e554c1
Melanie Treitinger <melanie.treitinger@ruhr-uni-bochum.de>
2024-10-24 15:53:15 +0200

    (wip) Add new fields to form and database

--- classes/local/entity/bookit_event.php
+++ classes/local/entity/bookit_event.php
@@ -41,105 +41,78 @@ class bookit_event {
     /** const STATUS_REJECTED */
     const STATUS_REJECTED = 3;
 
-    /** @var ?int id */
-    public ?int $id;
-    /** @var string name */
-    public string $name;
-    /** @var int semester */
-    public int $semester;
-    /** @var string department */
-    public string $department;
-    /** @var int starttime */
-    public int $starttime;
-    /** @var int endtime */
-    public int $endtime;
-    /** @var ?int duration */
-    public ?int $duration;
-    /** @var ?int participantsamount */
-    public ?int $participantsamount;
-    /** @var ?string compensationfordisadvantages */
-    public ?string $compensationfordisadvantages;
-    /** @var ?int status */
-    public ?int $status;
-    /** @var ?int personinchargeid */
-    public ?int $personinchargeid;
-    /** @var ?string personinchargename */
-    public ?string $personinchargename;
-    /** @var ?string personinchargeemail */
-    public ?string $personinchargeemail;
-    /** @var ?int coursetemplate */
-    public ?int $coursetemplate;
-    /** @var ?string notes */
-    public ?string $notes;
-    /** @var ?string internalnotes */
-    public ?string $internalnotes;
-    /** @var ?string support */
-    public ?string $support;
-    /** @var array resources */
-    public array $resources;
-    /** @var ?int refcourseid */
-    public ?int $refcourseid;
-    /** @var ?int usermodified */
-    public ?int $usermodified;
-    /** @var ?int timecreated */
-    public ?int $timecreated;
-    /** @var ?int timemodified */
-    public ?int $timemodified;
-
     /**
      * Create a new instance of this class.
      *
+     * @param int $id
      * @param string $name
-     * @param string $semester
+     * @param int|null $semester
      * @param string $department
      * @param int $starttime
      * @param int $endtime
-     * @param int $duration
-     * @param int $participantsamount
+     * @param int|null $duration
+     * @param int|null $participantsamount
+     * @param int|null $timecompensation
      * @param string|null $compensationfordisadvantages
-     * @param string $status
+     * @param int $bookingstatus
      * @param int|null $personinchargeid
-     * @param string $personinchargename
-     * @param string $personinchargeemail
-     * @param int $coursetemplate
+     * @param ?string $otherexaminers
+     * @param int|null $coursetemplate
      * @param string|null $notes
      * @param string|null $internalnotes
-     * @param string|null $support
+     * @param string|null $supportpersons
      * @param array $resources
      * @param int|null $refcourseid
      * @param int|null $usermodified
      * @param int|null $timecreated
      * @param int|null $timemodified
-     * @param int|null $id
      */
-    public function __construct(string $name, string $semester, string $department, int $starttime, int $endtime, int $duration,
-            int $participantsamount, string|null $compensationfordisadvantages, string $status, int|null $personinchargeid,
-            string $personinchargename,
-            string $personinchargeemail, int $coursetemplate, string|null $notes, string|null $internalnotes, string|null $support,
-            array $resources, int|null $refcourseid,
-            int|null $usermodified = null, int|null $timecreated = null, int|null $timemodified = null, int|null $id = null) {
-        $this->id = $id;
-        $this->name = $name;
-        $this->semester = $semester;
-        $this->department = $department;
-        $this->starttime = $starttime;
-        $this->endtime = $endtime;
-        $this->duration = $duration;
-        $this->participantsamount = $participantsamount;
-        $this->compensationfordisadvantages = $compensationfordisadvantages;
-        $this->status = $status;
-        $this->personinchargeid = $personinchargeid;
-        $this->personinchargename = $personinchargename;
-        $this->personinchargeemail = $personinchargeemail;
-        $this->coursetemplate = $coursetemplate;
-        $this->notes = $notes;
-        $this->internalnotes = $internalnotes;
-        $this->support = $support;
-        $this->resources = $resources;
-        $this->refcourseid = $refcourseid;
-        $this->usermodified = $usermodified;
-        $this->timecreated = $timecreated;
-        $this->timemodified = $timemodified;
+    public function __construct(
+        /** @var int id */
+        public int $id,
+        /** @var string name */
+        public string $name,
+        /** @var ?int semester */
+        public ?int $semester,
+        /** @var string department */
+        public string $department,
+        /** @var int starttime */
+        public int $starttime,
+        /** @var int endtime */
+        public int $endtime,
+        /** @var ?int duration */
+        public ?int $duration,
+        /** @var ?int participantsamount */
+        public ?int $participantsamount,
+        /** @var ?int timecompensation */
+        public ?int $timecompensation,
+        /** @var ?string compensationfordisadvantages */
+        public ?string $compensationfordisadvantages,
+        /** @var int bookingstatus */
+        public int $bookingstatus,
+        /** @var ?int personinchargeid */
+        public ?int $personinchargeid,
+        /** @var ?string otherexaminers */
+        public ?string $otherexaminers,
+        /** @var ?int coursetemplate */
+        public ?int $coursetemplate,
+        /** @var ?string notes */
+        public ?string $notes,
+        /** @var ?string internalnotes */
+        public ?string $internalnotes,
+        /** @var ?string supportpersons */
+        public ?string $supportpersons,
+        /** @var array resources */
+        public array $resources,
+        /** @var ?int refcourseid */
+        public ?int $refcourseid,
+        /** @var ?int usermodified */
+        public ?int $usermodified,
+        /** @var ?int timecreated */
+        public ?int $timecreated,
+        /** @var ?int timemodified */
+        public ?int $timemodified,
+    ) {
     }
 
     /**
@@ -179,15 +152,15 @@ class bookit_event {
                 $record->endtime,
                 $record->duration,
                 $record->participantsamount ?? null,
+                $record->timecompensation ?? null,
                 $record->compensationfordisadvantages ?? null,
-                $record->status,
+                $record->bookingstatus,
                 $record->personinchargeid ?? null,
-                $record->personinchargename,
-                $record->personinchargeemail,
+                $record->otherexaminers,
                 $record->coursetemplate ?? 0,
                 $record->notes ?? null,
                 $record->internalnotes ?? null,
-                $record->support ?? null,
+                $record->supportpersons ?? null,
                 $record->resources,
                 $record->refcourseid ?? null,
                 $record->usermodified ?? null,
@@ -204,7 +177,7 @@ class bookit_event {
      * @return void
      * @throws dml_exception
      */
-    public function save(int|null $userid = null): void {
+    final public function save(int|null $userid = null): void {
         global $DB, $USER;
         $this->usermodified = $userid ?? $USER->id;
         if (!$this->timecreated) {
