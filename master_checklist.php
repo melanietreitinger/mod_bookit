461a844dc3c20a4dc52a89c62d2bde607fb7716e
Andreas Rosenthal <arosenthal@ssystems.de>
2025-08-28 15:07:36 +0200

    add translations, fix ci

--- master_checklist.php
+++ master_checklist.php
@@ -1,6 +1,4 @@
 <?php
-
-use mod_bookit\local\manager\checklist_manager;
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -23,6 +21,9 @@ use mod_bookit\local\manager\checklist_manager;
  * @copyright  2025 Justus Dieckmann RUB
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+
+use mod_bookit\local\manager\checklist_manager;
+
 
 require_once(__DIR__ . '/../../config.php');
 require_once($CFG->libdir . '/adminlib.php');
