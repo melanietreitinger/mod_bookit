624fa1af2b319a408b805cc777ab8323827b612d
Melanie Treitinger <melanie.treitinger@ruhr-uni-bochum.de>
2025-11-13 13:31:46 +0100

    fix: version number

--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'mod_bookit';
 $plugin->version = 2325010209;
 $plugin->release = '0.1.0';
-$plugin->requires = 2024100700; // Moodle 4.5 (LTS).
+$plugin->requires = 2024100701; // Moodle 4.5 (LTS).
 $plugin->supported = [405, 406];
 $plugin->maturity = MATURITY_ALPHA;
