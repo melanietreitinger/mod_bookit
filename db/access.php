aa16691728d9d8e748bcb4c87eb4b2da4fdf638c
Melanie Treitinger <melanie.treitinger@ruhr-uni-bochum.de>
2025-11-19 09:21:12 +0100

    remove archetype role

--- db/access.php
+++ db/access.php
@@ -103,7 +103,6 @@ $capabilities = [
                 'captype' => 'write',
                 'contextlevel' => CONTEXT_SYSTEM,
                 'archetypes' => [
-                'manager' => CAP_ALLOW,
                 ],
         ],
 ];
